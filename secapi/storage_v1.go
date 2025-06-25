5115e17a5cdebba69c4c8fa3d22ce2549cd9fc20
Vincent Landgraf <vincent.landgraf@ionos.com>
2025-06-25 16:00:27 +0200

    generic crud example

--- secapi/storage_v1.go
+++ secapi/storage_v1.go
@@ -5,20 +5,47 @@ import (
 
 	"k8s.io/utils/ptr"
 
-	"github.com/eu-sovereign-cloud/go-sdk/pkg/spec/foundation.storage.v1"
+	storage "github.com/eu-sovereign-cloud/go-sdk/pkg/spec/foundation.storage.v1"
 )
 
 type StorageV1 struct {
 	storage storage.ClientWithResponsesInterface
+	crud    CRUD[
+		storage.BlockStorage,
+		storage.Tenant,
+		storage.Workspace,
+		storage.ListBlockStoragesParams,
+		storage.RequestEditorFn,
+		storage.ListBlockStoragesResponse,
+		storage.ResourceName,
+		storage.GetBlockStorageResponse,
+	]
 }
 
 func newStorageV1(storageUrl string) (*StorageV1, error) {
-	storage, err := storage.NewClientWithResponses(storageUrl)
+	s, err := storage.NewClientWithResponses(storageUrl)
 	if err != nil {
 		return nil, err
 	}
 
-	return &StorageV1{storage: storage}, nil
+	return &StorageV1{
+		storage: s,
+		crud: CRUD[storage.BlockStorage, storage.Tenant, storage.Workspace, storage.ListBlockStoragesParams, storage.RequestEditorFn, storage.ListBlockStoragesResponse, storage.ResourceName, storage.GetBlockStorageResponse]{
+			ReturnFn: func(resp *storage.ListBlockStoragesResponse) ([]storage.BlockStorage, *string, error) {
+				return resp.JSON200.Items, resp.JSON200.Metadata.SkipToken, nil
+			},
+			ReturnSingleFn: func(resp *storage.GetBlockStorageResponse) (*storage.BlockStorage, error) {
+				return resp.JSON200, nil
+			},
+			ParamsFn: func() *storage.ListBlockStoragesParams {
+				return &storage.ListBlockStoragesParams{
+					Accept: ptr.To(storage.ListBlockStoragesParamsAcceptApplicationjson),
+				}
+			},
+			ListFn: s.ListBlockStoragesWithResponse,
+			GetFn:  s.GetBlockStorageWithResponse,
+		},
+	}, nil
 }
 
 func validateStorageMetadataV1(metadata *storage.ZonalResourceMetadata) {
@@ -36,29 +63,11 @@ func validateStorageMetadataV1(metadata *storage.ZonalResourceMetadata) {
 }
 
 func (api *StorageV1) ListBlockStorages(ctx context.Context, tid TenantID, wid WorkspaceID) (*Iterator[storage.BlockStorage], error) {
-	iter := Iterator[storage.BlockStorage]{
-		fn: func(ctx context.Context, skipToken *string) ([]storage.BlockStorage, *string, error) {
-			resp, err := api.storage.ListBlockStoragesWithResponse(ctx, storage.Tenant(tid), storage.Workspace(wid), &storage.ListBlockStoragesParams{
-				Accept: ptr.To(storage.ListBlockStoragesParamsAcceptApplicationjson),
-			})
-			if err != nil {
-				return nil, nil, err
-			}
-
-			return resp.JSON200.Items, resp.JSON200.Metadata.SkipToken, nil
-		},
-	}
-
-	return &iter, nil
+	return api.crud.List(ctx, tid, wid)
 }
 
 func (api *StorageV1) GetBlockStorage(ctx context.Context, wref WorkspaceReference) (*storage.BlockStorage, error) {
-	resp, err := api.storage.GetBlockStorageWithResponse(ctx, storage.Tenant(wref.Tenant), storage.Workspace(wref.Workspace), wref.Name)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.JSON200, nil
+	return api.crud.Get(ctx, wref)
 }
 
 func (api *StorageV1) CreateOrUpdateBlockStorage(ctx context.Context, block *storage.BlockStorage) error {
